46d134836520f0d8c09e55b25ae9e649368a1d5e
brkhs906 <barhasbaajaa@gmail.com>
2023-07-07 12:59:38 +0800

    add:day 09

---
+++ day_09_flutter/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print('hello flutter');
+  runApp( const MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text('data')),
+    ),
+   ),
+  );
+}
+
+
