b7ba57da57e2436dfe8f0d0406dd18ae9225a101
brkhs906 <barhasbaajaa@gmail.com>
2023-07-24 12:21:52 +0800

    add: day_13

---
+++ day_13_flutter/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:word_find_app/basic_screen.dart';
+import 'package:word_find_app/immutable_widget.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BasicScreen(),
+  ));
+}
